10f57aca2e73e9b0caa799cf181f043259c87cf8
Kainan <kainanandrade10@gmail.com>
2021-08-21 05:31:46 -0300

    design patterns

---
+++ app/src/main/java/com/naniak/githubapi/features/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.naniak.githubapi.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naniak.githubapi.R
+import com.naniak.githubapi.databinding.ActivityMainBinding
+import com.naniak.githubapi.features.home.view.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/naniak/githubapi/features/home/view/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.naniak.githubapi.features.home.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.naniak.githubapi.R
+import com.naniak.githubapi.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private  var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+
+}
